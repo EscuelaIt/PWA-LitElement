1eb8f97615aa07672b429aa85c6c38e909fa80ef
Miguel Angel Alvarez <malvarez@desarrolloweb.com>
2019-08-01 18:37:22 +0200

    build primera fase

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,26 @@
 import { createDefaultConfig } from '@open-wc/building-rollup';
+import cpy from 'rollup-plugin-cpy';
 
 // if you need to support IE11 use "modern-and-legacy-config" instead.
 // import { createCompatibilityConfig } from '@open-wc/building-rollup';
 // export default createCompatibilityConfig({ input: './index.html' });
 
-export default createDefaultConfig({ input: './index.html' });
+const config = createDefaultConfig({ input: './index.html' });
+
+export default [
+  {
+    ...config,
+    plugins: [
+      ...config.plugins,
+      cpy({
+        // copy over all images files
+        files: ['**/*.png', '**/*.jpg', 'manifest.json'],
+        dest: 'dist',
+        options: {
+          // parents makes sure to preserve the original folder structure
+          parents: true
+        }
+      })
+    ]
+  }
+]
