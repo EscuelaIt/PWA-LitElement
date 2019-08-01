d8039db98a815fa6a73d529254c4cf66aa9ee4c7
Miguel Angel Alvarez <malvarez@desarrolloweb.com>
2019-08-01 19:06:49 +0200

    build para configuración legacy

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,35 @@
-import { createDefaultConfig } from '@open-wc/building-rollup';
-import cpy from 'rollup-plugin-cpy';
+//import { createDefaultConfig } from '@open-wc/building-rollup';
+//const config = createDefaultConfig({ input: './index.html' });
 
 // if you need to support IE11 use "modern-and-legacy-config" instead.
-// import { createCompatibilityConfig } from '@open-wc/building-rollup';
-// export default createCompatibilityConfig({ input: './index.html' });
+import { createCompatibilityConfig } from '@open-wc/building-rollup';
+import cpy from 'rollup-plugin-cpy';
 
-const config = createDefaultConfig({ input: './index.html' });
+const config = createCompatibilityConfig({ input: './index.html' });
+
+// export default [
+//   {
+//     ...config,
+//     plugins: [
+//       ...config.plugins,
+//       cpy({
+//         // copy over all images files
+//         files: ['**/*.png', '**/*.jpg', 'manifest.json', '_redirects'],
+//         dest: 'dist',
+//         options: {
+//           // parents makes sure to preserve the original folder structure
+//           parents: true
+//         }
+//       })
+//     ]
+//   }
+// ]
 
 export default [
   {
-    ...config,
+    ...config[0],
     plugins: [
-      ...config.plugins,
+      ...config[0].plugins,
       cpy({
         // copy over all images files
         files: ['**/*.png', '**/*.jpg', 'manifest.json', '_redirects'],
@@ -22,5 +40,7 @@ export default [
         }
       })
     ]
-  }
+  },
+  config[1]
 ]
+
