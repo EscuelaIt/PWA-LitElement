ba978a63cf9c4005b3ff9d85bebeb5fd7ea2e193
midesweb <malvarez@desarrolloweb.com>
2020-04-17 11:16:40 +0200

    actualizar dependencias

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,31 @@
-//import { createDefaultConfig } from '@open-wc/building-rollup';
-//const config = createDefaultConfig({ input: './index.html' });
+import merge from 'deepmerge';
+// use createSpaConfig for bundling a Single Page App
+import { createSpaConfig } from '@open-wc/building-rollup';
 
-// if you need to support IE11 use "modern-and-legacy-config" instead.
-import { createCompatibilityConfig } from '@open-wc/building-rollup';
-import cpy from 'rollup-plugin-cpy';
+// use createBasicConfig to do regular JS to JS bundling
+// import { createBasicConfig } from '@open-wc/building-rollup';
 
-const config = createCompatibilityConfig({ input: './index.html' });
+const baseConfig = createSpaConfig({
+  // use the outputdir option to modify where files are output
+  // outputDir: 'dist',
 
-// export default [
-//   {
-//     ...config,
-//     plugins: [
-//       ...config.plugins,
-//       cpy({
-//         // copy over all images files
-//         files: ['**/*.png', '**/*.jpg', 'manifest.json', '_redirects'],
-//         dest: 'dist',
-//         options: {
-//           // parents makes sure to preserve the original folder structure
-//           parents: true
-//         }
-//       })
-//     ]
-//   }
-// ]
+  // if you need to support older browsers, such as IE11, set the legacyBuild
+  // option to generate an additional build just for this browser
+  // legacyBuild: true,
 
-export default [
-  {
-    ...config[0],
-    plugins: [
-      ...config[0].plugins,
-      cpy({
-        // copy over all images files
-        files: ['**/*.png', '**/*.jpg', 'manifest.json', '_redirects', 'node_modules/axios/dist/axios.min.js', 'favicon.ico'],
-        dest: 'dist',
-        options: {
-          // parents makes sure to preserve the original folder structure
-          parents: true
-        }
-      })
-    ]
-  },
-  config[1]
-]
+  // development mode creates a non-minified build for debugging or development
+  developmentMode: process.env.ROLLUP_WATCH === 'true',
 
+  // set to true to inject the service worker registration into your index.html
+  injectServiceWorker: false,
+});
+
+export default merge(baseConfig, {
+  // if you use createSpaConfig, you can use your index.html as entrypoint,
+  // any <script type="module"> inside will be bundled by rollup
+  input: './index.html',
+
+  // alternatively, you can use your JS as entrypoint for rollup and
+  // optionally set a HTML template manually
+  // input: './app.js',
+});
